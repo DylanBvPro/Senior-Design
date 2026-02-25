5c946e9d6b3a13cc40e31e623efeae1cbf86e3ce
Dylan Bv <89555477+DylanBvPro@users.noreply.github.com>
2026-02-25 00:45:51 -0500

    Minor Update
    1. Fixed issues for in person showcase.

--- bit-forge/camera_3d.gd
+++ bit-forge/camera_3d.gd
@@ -8,7 +8,7 @@ var start_rotation := 0.0
 func _ready():
 	start_rotation = rotation.y
 
-func _process(delta):
+func _process(_delta):
 	var angle = sin(Time.get_ticks_msec() * 0.001 * rotation_speed)
 	var half_range = deg_to_rad(rotation_range_degrees / 2.0)
 	rotation.y = start_rotation + angle * half_range
